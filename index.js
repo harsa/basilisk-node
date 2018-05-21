e4d2baedb81f1bb3b317b21e59d64fd134d8fa89
Harri Hurme <harri.hurme@iki.fi>
2018-05-21 19:53:03 +0300

    https, baby

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
-var http = require('http');
+var http = require("http");
 var express = require("express");
 var RED = require("node-red");
-
+const https = require('https')
+const fs = require('fs')
 // Create an Express app
 var app = express();
 
 // Add a simple route for static content served from 'public'
-app.use("/",express.static("public"));
+app.use("/", express.static("public"));
 
 // Create a server
-var server = http.createServer(app);
+var server = https.createServer(
+  {
+    key: fs.readFileSync("./cert/localhost.key"),
+    cert: fs.readFileSync("./cert/localhost.crt")
+  },
+  app
+);
 
 // Create the settings object - see default settings.js file for other options
 var settings = {
-	httpAdminRoot:"/red",
-	httpNodeRoot: "/api",
-	//userDir:"/home/pi/basilisk/.node-red",
-	userDir:".node-red",
-	functionGlobalContext: { } ,   // enables global context
-	flowFile: "flows.json",
+  httpAdminRoot: "/red",
+  httpNodeRoot: "/api",
+  //userDir:"/home/pi/basilisk/.node-red",
+  userDir: ".node-red",
+  functionGlobalContext: {}, // enables global context
+  flowFile: "flows.json"
 };
 
 // Initialise the runtime with a server and settings
-RED.init(server,settings);
+RED.init(server, settings);
 
 // Serve the editor UI from /red
-app.use(settings.httpAdminRoot,RED.httpAdmin);
+app.use(settings.httpAdminRoot, RED.httpAdmin);
 
 // Serve the http nodes UI from /api
-app.use(settings.httpNodeRoot,RED.httpNode);
+app.use(settings.httpNodeRoot, RED.httpNode);
 
-app.use(express.static('build'))
+app.use(express.static("build"));
 
 server.listen(8000);
 
